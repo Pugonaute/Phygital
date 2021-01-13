015f5d6583a9555cae026c6f29cf3ce324653a9d
Pugonaute <60344571+Pugonaute@users.noreply.github.com>
2021-01-13 12:18:09 +0100

    menu & img

--- index.php
+++ index.php
@@ -7,7 +7,18 @@
     <title>L'éthiquette</title>
 </head>
 <body>
-
+    <header>
+        <img src="img/logo-ethiquette.svg" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="">Application</a></li>
+                <li><a href="">Devenir partenaire</a></li>
+                <li><a href="">A propos</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Contactez-nous</a></li>
+            </ul>
+        </nav>
+    </header>
     <H1>Hello World</H1>
     
 </body>
