f8a0a9ad0297143ecd21c1a706ce99ab62f4cbb8
Alex <alex.c@posteo.net>
2021-01-18 01:37:41 +0100

    footer et fonts

--- index.php
+++ index.php
@@ -1,26 +1,113 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>L'éthiquette</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <img src="img/logo-ethiquette-blanc.svg" alt="logo">
-            <hr>
-            <ul>
-                <li><a href="">Application</a></li>
-                <li><a href="">Devenir partenaire</a></li>
-                <li><a href="">A propos</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">Contactez-nous</a></li>
-            </ul>
-        </nav>
-    </header>
-    <H1>Hello World</H1>
-    
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/style.css">
+		<title>L'éthiquette</title>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<img src="img/logo-ethiquette-blanc.svg" alt="logo">
+				<hr>
+				<ul>
+					<li><a href="">Application</a></li>
+					<li><a href="">Devenir partenaire</a></li>
+					<li><a href="">A propos</a></li>
+					<li><a href="">Blog</a></li>
+					<li><a href="">Contactez-nous</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<H1>Hello World</H1>
+			<!--
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi error alias, quia in quod tempora suscipit quibusdam nihil praesentium iste culpa assumenda, a non ea quasi asperiores voluptatum similique. Nihil sapiente possimus nobis, amet molestiae eligendi accusamus dolore magnam, at dignissimos deserunt officiis dolorem doloremque, molestias explicabo. Deserunt cumque expedita incidunt velit beatae sunt recusandae quasi officiis iste, aut perspiciatis. Hic reprehenderit quasi illum, quibusdam distinctio modi, magni laudantium sequi? Quas quisquam rem vel. Dignissimos, maxime aperiam eum suscipit placeat nostrum nihil consequatur. Provident laborum, non aperiam similique soluta odit recusandae ipsam harum natus sint sit molestias nemo! Aut, quam!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi error alias, quia in quod tempora suscipit quibusdam nihil praesentium iste culpa assumenda, a non ea quasi asperiores voluptatum similique. Nihil sapiente possimus nobis, amet molestiae eligendi accusamus dolore magnam, at dignissimos deserunt officiis dolorem doloremque, molestias explicabo. Deserunt cumque expedita incidunt velit beatae sunt recusandae quasi officiis iste, aut perspiciatis. Hic reprehenderit quasi illum, quibusdam distinctio modi, magni laudantium sequi? Quas quisquam rem vel. Dignissimos, maxime aperiam eum suscipit placeat nostrum nihil consequatur. Provident laborum, non aperiam similique soluta odit recusandae ipsam harum natus sint sit molestias nemo! Aut, quam!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi error alias, quia in quod tempora suscipit quibusdam nihil praesentium iste culpa assumenda, a non ea quasi asperiores voluptatum similique. Nihil sapiente possimus nobis, amet molestiae eligendi accusamus dolore magnam, at dignissimos deserunt officiis dolorem doloremque, molestias explicabo. Deserunt cumque expedita incidunt velit beatae sunt recusandae quasi officiis iste, aut perspiciatis. Hic reprehenderit quasi illum, quibusdam distinctio modi, magni laudantium sequi? Quas quisquam rem vel. Dignissimos, maxime aperiam eum suscipit placeat nostrum nihil consequatur. Provident laborum, non aperiam similique soluta odit recusandae ipsam harum natus sint sit molestias nemo! Aut, quam!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi error alias, quia in quod tempora suscipit quibusdam nihil praesentium iste culpa assumenda, a non ea quasi asperiores voluptatum similique. Nihil sapiente possimus nobis, amet molestiae eligendi accusamus dolore magnam, at dignissimos deserunt officiis dolorem doloremque, molestias explicabo. Deserunt cumque expedita incidunt velit beatae sunt recusandae quasi officiis iste, aut perspiciatis. Hic reprehenderit quasi illum, quibusdam distinctio modi, magni laudantium sequi? Quas quisquam rem vel. Dignissimos, maxime aperiam eum suscipit placeat nostrum nihil consequatur. Provident laborum, non aperiam similique soluta odit recusandae ipsam harum natus sint sit molestias nemo! Aut, quam!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi error alias, quia in quod tempora suscipit quibusdam nihil praesentium iste culpa assumenda, a non ea quasi asperiores voluptatum similique. Nihil sapiente possimus nobis, amet molestiae eligendi accusamus dolore magnam, at dignissimos deserunt officiis dolorem doloremque, molestias explicabo. Deserunt cumque expedita incidunt velit beatae sunt recusandae quasi officiis iste, aut perspiciatis. Hic reprehenderit quasi illum, quibusdam distinctio modi, magni laudantium sequi? Quas quisquam rem vel. Dignissimos, maxime aperiam eum suscipit placeat nostrum nihil consequatur. Provident laborum, non aperiam similique soluta odit recusandae ipsam harum natus sint sit molestias nemo! Aut, quam!</p>
+
+-->
+		</section>
+		<footer>
+			<div>
+				<div>	
+					<div>
+						<img src="img/logo-ethiquette-blanc.svg" alt="logo">
+					</div>
+					<div>
+						<button>Google Play</button>
+						<button>Apple Store</button>
+					</div>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="">À propos</a>
+							<ul>
+								<li>
+									<a href="">Manifest</a>
+								</li>
+								<li>
+									<a href="">Nos méthodes</a>
+								</li>
+								<li>
+									<a href="">L'équipe</a>
+								</li>
+								<li>
+									<a href="">Marques partenaires</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<hr>
+					<ul>
+						<li><a href="">Nos services</a>
+							<ul>
+								<li>
+									<a href="">Application</a>
+								</li>
+								<li>
+									<a href="">Fonctionnalités</a>
+								</li>
+								<li>
+									<a href="">Devenir commerçant</a>
+								</li>
+								<li>
+									<a href="">Stratégie</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<hr>
+					<ul>
+						<li><a href="">Liens utiles</a>
+							<ul>
+								<li>
+									<a href="">Besoin d'aide ?</a>
+								</li>
+								<li>
+									<a href="">Nous contacter</a>
+								</li>
+								<li>
+									<a href="">Blog</a>
+								</li>
+								<li>
+									<a href="">CGU CGV</a>
+								</li>
+								<li>
+									<a href="">Réseaux</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<hr>
+			<div>
+				© L’éthiquette - 2021
+			</div>
+		</footer>
+	</body>
 </html>
