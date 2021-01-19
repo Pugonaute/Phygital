ffce5427b7edbaa9092b708104c9679ff12a5e08
Alex <alex.c@posteo.net>
2021-01-19 15:49:15 +0100

    fix footer

--- index.php
+++ index.php
@@ -1,43 +1,118 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>L'éthiquette</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <img src="img/logo-ethiquette-blanc.svg" alt="logo">
-            <hr>
-            <ul>
-                <li><a href="">Application</a></li>
-                <li><a href="">Devenir partenaire</a></li>
-                <li><a href="">A propos</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">Contactez-nous</a></li>
-            </ul>
-        </nav>
-    </header>
-    
-    <section id="hero">
-        <div class="conteneur">
-            <h1>Habillez vous éthique, devenez acteur pour la planète</h1>
-            <h2>Il vous suffit de scanner !</h2>
-            <p>L’ethiquette, l’application mobile qui vous permet de décrypter la composition de vos vêtements pour évaluer leur impact sur la planète et ainsi agir pour une mode plus responsable et locale.</p>
-            
-            <a class="button" href="">Regarder la vidéo</a>
-        </div>
-    </section>
-    <section id="presentation">
-        <div class="conteneur">
-            <h2>Découvrez notre application</h2>
-            <div class="video"></div>
-        </div>
-    </section>
-        
-    
-    
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/style.css">
+		<title>L'éthiquette</title>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<img src="img/logo-ethiquette-blanc.svg" alt="logo">
+				<hr>
+				<ul>
+					<li><a href="">Application</a></li>
+					<li><a href="">Devenir partenaire</a></li>
+					<li><a href="">A propos</a></li>
+					<li><a href="">Blog</a></li>
+					<li><a href="">Contactez-nous</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		<section id="hero">
+			<div class="conteneur">
+				<h1>Habillez vous éthique, devenez acteur pour la planète</h1>
+				<h2>Il vous suffit de scanner !</h2>
+				<p>L’ethiquette, l’application mobile qui vous permet de décrypter la composition de vos vêtements pour évaluer leur impact sur la planète et ainsi agir pour une mode plus responsable et locale.</p>
+
+				<a class="button" href="">Regarder la vidéo</a>
+			</div>
+		</section>
+		<section id="presentation">
+			<div class="conteneur">
+				<h2>Découvrez notre application</h2>
+				<div class="video"></div>
+			</div>
+		</section>
+		<footer>
+			<div>
+				<div>	
+					<div>
+						<img src="img/logo-ethiquette-blanc.svg" alt="logo">
+					</div>
+					<div>
+						<button>Google Play</button>
+						<button>Apple Store</button>
+					</div>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="">À propos</a>
+							<ul>
+								<li>
+									<a href="">Manifest</a>
+								</li>
+								<li>
+									<a href="">Nos méthodes</a>
+								</li>
+								<li>
+									<a href="">L'équipe</a>
+								</li>
+								<li>
+									<a href="">Marques partenaires</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<hr>
+					<ul>
+						<li><a href="">Nos services</a>
+							<ul>
+								<li>
+									<a href="">Application</a>
+								</li>
+								<li>
+									<a href="">Fonctionnalités</a>
+								</li>
+								<li>
+									<a href="">Devenir commerçant</a>
+								</li>
+								<li>
+									<a href="">Stratégie</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<hr>
+					<ul>
+						<li><a href="">Liens utiles</a>
+							<ul>
+								<li>
+									<a href="">Besoin d'aide ?</a>
+								</li>
+								<li>
+									<a href="">Nous contacter</a>
+								</li>
+								<li>
+									<a href="">Blog</a>
+								</li>
+								<li>
+									<a href="">CGU CGV</a>
+								</li>
+								<li>
+									<a href="">Réseaux</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<hr>
+			<div>
+				© L’éthiquette - 2021
+			</div>
+		</footer>
+	</body>
 </html>
