10c2c7b9c94d2943da4b44cba9e15f2921d67a5c
Lambd-Alex <71758606+Lambd-Alex@users.noreply.github.com>
2021-02-04 19:51:31 +0100

    add article

---
+++ pages/articles/consigne-ethique.php
@@ -0,0 +1,73 @@
+<?php
+$root = '../../';
+
+// Title à remplir
+$title = 'Consigne : le nouveau pari des marques de mode éthique';
+
+// TO DO : description à faire
+$desc = '';
+
+include $root."php/inc/header.inc.php";
+?>
+<main id="article-001">
+	<div class="conteneur">
+		<div>
+			<h1>Consigne : le nouveau pari des marques <span>de mode éthique</span></h1>
+		</div>
+		<section class="article">
+			<div>
+				<img src="<?= $root ?>img/header_art-001.png" alt="">
+			</div>
+			<div class="info">
+				<div>
+					<p>Par John Doe - 20 NOVEMBRE 2020</p>
+				</div>
+				<div class=buttonIcon>
+					<p>Impression</p>
+				</div>
+				<div class=buttonIcon>
+					<p>Partager</p>
+				</div>
+			</div>
+			<article>
+				<p class=strong>
+					Comme un pied de nez au Black Friday, devenu synonyme d’incitation à la surconsommation, Hopaal, 1083, Atelier Unes et Panafrica ont choisi ce moment de l'année pour dire stop à la mode jetable et promouvoir dans leur collection le système de consigne.
+				</p>
+				<p>
+					« Fermer la boucle d’un article que l’on a produit et vendu est l’une de nos préoccupations en tant qu’acteur de la mode, explique Clément Maulavé, co-fondateur et président de la marque Hopaal. Mais celle-ci est commune à tout l’éco-système de la mode responsable avec qui nous partageons les mêmes préoccupations, C’est pourquoi nous prenons aujourd’hui la parole de concert pour gagner en visibilité et parce que l’entraide fait avancer les choses. »
+				</p>
+				<p>
+					Et si 1083 a ouvert la voie, en lançant le jean Infini en 2019, le premier modèle en matières recyclées et recyclables (119 €) que l’on peut retourner gratuitement à la marque en fin de vie pour qu’il serve à en fabriquer un nouveau et se faire rembourser sa consigne (20 € pour le modèle), aujourd’hui, d’autres confrères prennent le même chemin. Ainsi, Hopaal propose depuis début 2020, la veste Infinie (205 €) en collaboration avec 1083. « Nous avons utilisé la même toile de denim recyclée et recyclable que 1083 mais avons dessiné notre propre design, souligne Clément Maulavé. Le prix de la consigne (20 €) a été fixé par nos clients car nous travaillons beaucoup en co-création avec eux, c’est notre fonctionnement. »
+				</p>
+				<div>
+					<img src="<?=$root;?>img/article-001_joie.png" alt="">
+				</div>
+				<p>
+					Comme un pied de nez au Black Friday, devenu synonyme d’incitation à la surconsommation, Hopaal, 1083, Atelier Unes et Panafrica ont choisi ce moment de l'année pour dire stop à la mode jetable et promouvoir dans leur collection le système de consigne.
+				</p>
+				<p>
+					« Fermer la boucle d’un article que l’on a produit et vendu est l’une de nos préoccupations en tant qu’acteur de la mode, explique Clément Maulavé, co-fondateur et président de la marque Hopaal. Mais celle-ci est commune à tout l’éco-système de la mode responsable avec qui nous partageons les mêmes préoccupations, C’est pourquoi nous prenons aujourd’hui la parole de concert pour gagner en visibilité et parce que l’entraide fait avancer les choses. »
+				</p>
+				<p>
+					Et si 1083 a ouvert la voie, en lançant le jean Infini en 2019, le premier modèle en matières recyclées et recyclables (119 €) que l’on peut retourner gratuitement à la marque en fin de vie pour qu’il serve à en fabriquer un nouveau et se faire rembourser sa consigne (20 € pour le modèle), aujourd’hui, d’autres confrères prennent le même chemin. Ainsi, Hopaal propose depuis début 2020, la veste Infinie (205 €) en collaboration avec 1083. « Nous avons utilisé la même toile de denim recyclée et recyclable que 1083 mais avons dessiné notre propre design, souligne Clément Maulavé. Le prix de la consigne (20 €) a été fixé par nos clients car nous travaillons beaucoup en co-création avec eux, c’est notre fonctionnement. »
+				</p>
+			</article>
+			<div class=like>
+				<div>
+					<p>Avez-vous aimez cet article ?<br>
+					189 utilisateurs ont aimé cet article.
+					</p>
+				</div>
+				<div class=buttonIcon>
+					<p>Oui</p>
+				</div>
+				<div class=buttonIcon>
+					<p>Non</p>
+				</div>
+			</div>
+		</section>
+	</div>
+</main>
+<?php
+include $root.'php/inc/footer.inc.php';
+?>
