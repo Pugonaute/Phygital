4fd9a966b52c1e4ed7e4b14e0adba93dd6602df1
Lambd-Alex <71758606+Lambd-Alex@users.noreply.github.com>
2021-02-23 09:17:16 +0100

    contact responsive + variables + en-tête - responsive

--- pages/contact.php
+++ pages/contact.php
@@ -1,64 +1,62 @@
-    <?php
+<?php
+	$root = '../'; 
 
-        $root = '../'; 
+	// Title à remplir
+	$title = 'Contact';
 
-        // Title à remplir
-        $title = 'Contact';
+	// TO DO : description à faire
+	$desc = '';
 
-        // TO DO : description à faire
-        $desc = '';
-
-        include $root."/php/inc/header.inc.php";
-    ?>
+	include $root."/php/inc/header.inc.php";
+?>
 
 <main id="contact">
-    <section id="formulaire">
-        <div class="conteneur">
-            <h1>Vous souhaitez<br>nous contacter ?</h1>
-            <h4>Nous sommes là pour vous !</h4>
-            <article>
-                <ul>
-                    <li>Je suis un commerçant</li>
-                    <li>Je suis une marque</li>
-                    <li>Je suis un utilisateur</li>
-                </ul>
-                <ul>
-                    <li>Raison de votre prise de contact
-                        <ul>
-                            <li>J'ai besoin d'aide sur l'application</li>
-                            <li>Je veux transmettre les données de mes vêtements</li>
-                            <li>J'ai un problème avec ma commande</li>
-                            <li>Comment récupérer ma commande</li>
-                            <li>Je veux devenir partenaire</li>
-                            <li>Je veux devenir "Ethique First"</li>
-                            <li>Autre</li>
-                        </ul>
-                    </li>
-                    <div></div>
-                </ul>
-                <ul>
-                    <li>Nom</li>
-                    <li>Prénom</li>
-                </ul>
-                <ul>
-                    <li>Entreprise</li>
-                </ul>
-                <ul>
-                    <li>Message</li>
-                </ul>
-                <ul>
-                    <li>Choisir un fichier</li>
-                    <li>Aucune pièce-jointe</li>
-                </ul>
-            </article>
-            <a href="" class="button1">
-                <div><span>Envoyer le message</div></span>
-            </a>
-            
-        </div>
-    </section>
+	<section id="formulaire">
+		<div class="conteneur">
+			<h1>Vous souhaitez<br>nous contacter ?</h1>
+			<h4>Nous sommes là pour vous !</h4>
+			<article>
+				<ul>
+					<li>Je suis un commerçant</li>
+					<li>Je suis une marque</li>
+					<li>Je suis un utilisateur</li>
+				</ul>
+				<ul>
+					<li>Raison de votre prise de contact
+						<ul>
+							<li>J'ai besoin d'aide sur l'application</li>
+							<li>Je veux transmettre les données de mes vêtements</li>
+							<li>J'ai un problème avec ma commande</li>
+							<li>Comment récupérer ma commande</li>
+							<li>Je veux devenir partenaire</li>
+							<li>Je veux devenir "Ethique First"</li>
+							<li>Autre</li>
+						</ul>
+					</li>
+					<div></div>
+				</ul>
+				<ul>
+					<li>Nom</li>
+					<li>Prénom</li>
+				</ul>
+				<ul>
+					<li>Entreprise</li>
+				</ul>
+				<ul>
+					<li>Message</li>
+				</ul>
+				<ul>
+					<li>Choisir un fichier</li>
+					<li>Aucune pièce-jointe</li>
+				</ul>
+			</article>
+			<a href="" class="button1">
+				<div><span>Envoyer le message</span></div>
+			</a>
+		</div>
+	</section>
 </main>
-
-    <?php   
-        include '../php/inc/footer.inc.php';
-    ?>
+<script src="<?= $root ?>js/menu.js"></script>
+<?php   
+	include '../php/inc/footer.inc.php';
+?>
